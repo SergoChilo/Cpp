7f3e72097baf99eba655209f35df7aaaa3cf4a79
SergoChilo <chilingaryan1997@internet.ru>
2022-04-21 09:44:03 +0400

    Dynamic memory array example

--- week_1/dynamic_array_example.cpp
+++ week_1/dynamic_array_example.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
+
 using namespace std;
 
-int main(){
-    int size = 5;
+int main() {
+    int size = 0;
+    cout << "Enter array size : ";
     cin >> size;
     int *arr = new int[size];
-    delete [] arr;
-    return 0;
+
+    //Add array from 0 to 9
+    for (int i = 0; i < size; i++) {
+        arr[i] = rand() % 10;
+    }
+
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << "\t" << arr + i << endl;
+    }
+
+    delete[] arr;
 }
